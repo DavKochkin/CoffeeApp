cd61a74f3df905a9bfdf8b178c01bb11fa00bbb5
David Kochkin <kochkin.david@gmail.com>
2023-07-26 20:40:05 +0300

    Scroll View Styling

--- CoffeeApp/Home/HomeViewController.swift
+++ CoffeeApp/Home/HomeViewController.swift
@@ -11,85 +11,97 @@ class HomeViewController: CoffeeAppViewController {
     
     let headerView = HomeHeaderView()
     let scrollView = UIScrollView()
-    let stackView = UIStackView()
+    let rootStackView = UIStackView()
     
-    var headerViewTopConstraints: NSLayoutConstraint?
+    var headerViewTopConstraint: NSLayoutConstraint?
     
     let tiles = [
-        TileView("Star balance"),
-        TileView("Bonus Stars"),
-        TileView("Try these"),
-        TileView("Welcome back"),
-        TileView("Uplifting")
-    ]
-    
+                TileView("Star balance"),
+                TileView("Bonus stars"),
+                TileView("Try these"),
+                TileView("Welcome back"),
+                TileView("Uplifting")
+                ]
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupScrollView()
-        setTabBarImage(imageName: "house.fill", title: "Home")
-        
-        style()
         layout()
-        
-        if #available(iOS 15.0, *) {
-            navigationController?.tabBarController?.tabBar.scrollEdgeAppearance = navigationController?.tabBarController?.tabBar.standardAppearance
-        }
+        setupScrollView()
+    }
+    
+    override func commonInit() {
+        setTabBarImage(imageName: "house.fill", title: "Home")
     }
     
     func setupScrollView() {
         scrollView.delegate = self
     }
+    
 }
 
-
+// MARK: Layout
 extension HomeViewController {
-    func style() {
+    func layout() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
         headerView.backgroundColor = .systemPink
-        
-        scrollView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        stackView.axis = .vertical
-        stackView.spacing = 8
-    }
-    
-    func layout() {
-        view.addSubview(headerView)
-        
-        headerViewTopConstraints = headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
-        
-        NSLayoutConstraint.activate([
 
-            headerViewTopConstraints!,
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        rootStackView.translatesAutoresizingMaskIntoConstraints = false
+        
+        rootStackView.axis = .vertical
+        rootStackView.spacing = 8
+        
+        view.addSubview(headerView)
+        view.addSubview(scrollView)
+        
+        scrollView.addSubview(rootStackView)
+        
+        for tile in tiles {
+            addChild(tile)
+            rootStackView.addArrangedSubview(tile.view)
+            tile.didMove(toParent: self)
+        }
+        
+        headerViewTopConstraint = headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
+
+        NSLayoutConstraint.activate([
+            headerViewTopConstraint!,
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             
+            scrollView.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: 8),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 8),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -8),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             
-            
+            rootStackView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            rootStackView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            rootStackView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            rootStackView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            rootStackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor),
         ])
-       
     }
 }
 
-
-//MARK: Animating scrollView
+// MARK: Animating scrollView
 extension HomeViewController: UIScrollViewDelegate {
-    
+        
+    // Snap to position
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let y = scrollView.contentOffset.y
-        
-        let swipinDown = y <= 0
+
+        let swipingDown = y <= 0
         let shouldSnap = y > 30
-        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
-        
+        let labelHeight = headerView.greeting.frame.height + 16
+
         UIView.animate(withDuration: 0.3) {
-            self.headerView.greeting.alpha = swipinDown ? 1.0 : 0.0
+            self.headerView.greeting.alpha = swipingDown ? 1.0 : 0.0
         }
-        
-        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: []) {
-            self.headerViewTopConstraints?.constant = shouldSnap ? -labelHeight : 0
+
+        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: [], animations: {
+            self.headerViewTopConstraint?.constant = shouldSnap ? -labelHeight : 0
             self.view.layoutIfNeeded()
-        }
+        })
     }
+    
 }
--- CoffeeApp/Home/TileView.swift
+++ CoffeeApp/Home/TileView.swift
@@ -15,28 +15,26 @@ class TileView: UIViewController {
         super.init(nibName: nil, bundle: nil)
         label.text = text
     }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .systemCyan
+        view.backgroundColor = .systemYellow
         layout()
+    }
+        
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func layout() {
         label.translatesAutoresizingMaskIntoConstraints = false
         
         view.addSubview(label)
+        
         NSLayoutConstraint.activate([
             label.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-            label.heightAnchor.constraint(equalToConstant: 300)
-            
+            view.heightAnchor.constraint(equalToConstant: 300)
         ])
-        
     }
 }
