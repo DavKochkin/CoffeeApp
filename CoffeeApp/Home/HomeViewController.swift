eac148026dcba61aebab3f4d05ecd5d45d6ede78
David Kochkin <kochkin.david@gmail.com>
2023-07-26 20:01:17 +0300

    Add the tile

--- CoffeeApp/Home/HomeViewController.swift
+++ CoffeeApp/Home/HomeViewController.swift
@@ -12,8 +12,6 @@ class HomeViewController: CoffeeAppViewController {
     let headerView = HomeHeaderView()
     var headerViewTopConstraints: NSLayoutConstraint?
     
-    var tableView = UITableView()
-    
     let cellId = "cellId"
     let tiles = [
         "Star balance",
@@ -26,7 +24,6 @@ class HomeViewController: CoffeeAppViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setTabBarImage(imageName: "house.fill", title: "Home")
-        setupTableView()
         
         style()
         layout()
@@ -42,26 +39,18 @@ extension HomeViewController {
     func style() {
         headerView.translatesAutoresizingMaskIntoConstraints = false
         headerView.backgroundColor = .systemPink
-        
-        tableView.translatesAutoresizingMaskIntoConstraints = false
     }
     
     func layout() {
         view.addSubview(headerView)
-        view.addSubview(tableView)
         
         headerViewTopConstraints = headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor)
         
         NSLayoutConstraint.activate([
-        //    headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+
             headerViewTopConstraints!,
             headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            
-            tableView.topAnchor.constraint(equalTo: headerView.bottomAnchor),
-            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
             
         ])
        
@@ -69,54 +58,27 @@ extension HomeViewController {
 }
 
 
-//MARK: TableView
-
-extension HomeViewController: UITableViewDataSource {
-    
-    func setupTableView() {
-        tableView.dataSource = self
-        tableView.delegate = self
-        
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-        tableView.tableFooterView = UIView()
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
-        
-        cell.textLabel?.text = tiles[indexPath.row]
-        cell.accessoryType = UITableViewCell.AccessoryType.disclosureIndicator
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return tiles.count
-    }
-}
-
-
 //MARK: Animating scrollView
-extension HomeViewController: UITableViewDelegate {
+
     
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 300
-    }
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return 300
+//    }
     
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        let y = scrollView.contentOffset.y
-        
-        let swipinDown = y <= 0
-        let shouldSnap = y > 30
-        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
-        
-        UIView.animate(withDuration: 0.3) {
-            self.headerView.greeting.alpha = swipinDown ? 1.0 : 0.0
-        }
-        
-        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: []) {
-            self.headerViewTopConstraints?.constant = shouldSnap ? -labelHeight : 0
-            self.view.layoutIfNeeded()
-        }
-    }
-}
+//    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+//        let y = scrollView.contentOffset.y
+//
+//        let swipinDown = y <= 0
+//        let shouldSnap = y > 30
+//        let labelHeight = headerView.greeting.frame.height + 16 // label + spacer (102)
+//
+//        UIView.animate(withDuration: 0.3) {
+//            self.headerView.greeting.alpha = swipinDown ? 1.0 : 0.0
+//        }
+//
+//        UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: []) {
+//            self.headerViewTopConstraints?.constant = shouldSnap ? -labelHeight : 0
+//            self.view.layoutIfNeeded()
+//        }
+//    }
+
