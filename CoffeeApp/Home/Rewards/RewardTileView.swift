afbc2ccbb887025c243264a0b3c9509020a9ec3b
David Kochkin <kochkin.david@gmail.com>
2023-07-27 11:56:25 +0300

    Adding the other elements

--- CoffeeApp/Home/Rewards/RewardTileView.swift
+++ CoffeeApp/Home/Rewards/RewardTileView.swift
@@ -10,6 +10,11 @@ import UIKit
 class RewardTileView: UIView {
     
     let balanceView = BalanceView()
+    var rewardsButtons = UIButton()
+    let rewardsGraphView = UIView()
+    let starRewardsView = UIView()
+    var detailsButton = UIButton()
+    
     
     override init(frame: CGRect) {
         super.init(frame: .zero)
@@ -29,14 +34,46 @@ class RewardTileView: UIView {
 extension RewardTileView {
     func style() {
         balanceView.translatesAutoresizingMaskIntoConstraints = false
+        rewardsGraphView.translatesAutoresizingMaskIntoConstraints = false
+        starRewardsView.translatesAutoresizingMaskIntoConstraints = false
+        
+        rewardsButtons.translatesAutoresizingMaskIntoConstraints = false
+        detailsButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        rewardsButtons.titleLabel?.text = "Rewards options"
+        detailsButton.titleLabel?.text = "Details"
+        
+        rewardsButtons.backgroundColor = .systemBlue
+        detailsButton.backgroundColor = .systemBlue
+        
+        
     }
     
     func layout() {
         addSubview(balanceView)
+        addSubview(rewardsButtons)
+        addSubview(rewardsGraphView)
+        addSubview(starRewardsView)
+        addSubview(detailsButton)
         
         NSLayoutConstraint.activate([
             balanceView.topAnchor.constraint(equalTo: topAnchor),
             balanceView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 2),
+            
+            rewardsButtons.centerYAnchor.constraint(equalTo: balanceView.pointsLabel.centerYAnchor),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: rewardsButtons.trailingAnchor, multiplier: 3),
+            
+            rewardsGraphView.topAnchor.constraint(equalToSystemSpacingBelow: balanceView.bottomAnchor, multiplier: 1),
+            rewardsGraphView.centerXAnchor.constraint(equalTo: centerXAnchor),
+            rewardsGraphView.widthAnchor.constraint(equalToConstant: frame.width),
+            
+            starRewardsView.topAnchor.constraint(equalTo: rewardsGraphView.bottomAnchor, constant: 8),
+            starRewardsView.leadingAnchor.constraint(equalToSystemSpacingAfter: leadingAnchor, multiplier: 1),
+            trailingAnchor.constraint(equalToSystemSpacingAfter: starRewardsView.trailingAnchor, multiplier: 1),
+            
+            detailsButton.topAnchor.constraint(equalToSystemSpacingBelow: starRewardsView.bottomAnchor, multiplier: 2),
+            detailsButton.leadingAnchor.constraint(equalTo: balanceView.leadingAnchor),
+            bottomAnchor.constraint(equalToSystemSpacingBelow: detailsButton.bottomAnchor, multiplier: 2),
             
         ])
     }
